e85d1ade37c84677be5825123a7b8950f9eb1946
Alec <38861973+ScalarShift@users.noreply.github.com>
2019-02-18 10:43:12 -0800

    Add solution to problem 10

---
+++ PEProblem10/scripts.js
@@ -0,0 +1,56 @@
+function factor(number){
+  
+  if (number === 1) {return number};
+
+  let factors = [];
+
+  let limit = Math.sqrt(number)
+
+  for (i=1; i<=limit; i++){
+
+    if (number%i === 0) {
+
+      factors.push(i);
+
+      if (i !== number/i) {
+
+        factors.push(number/i);
+
+      }
+
+    }
+
+  }
+
+  return factors;
+
+}
+
+function isPrime(number){
+
+  let factors = factor(number);
+
+  return (factors.length == 2 && number != 1);
+
+}
+
+function sumOfPrimes(limit){
+  let currentSum = 0;
+  for(let i=2; i<limit; i++){
+    if (isPrime(i)) {currentSum += i;}
+  }
+  return currentSum;
+}
+
+//-------------------------------------------------
+
+let numberBox = document.getElementById("number");
+let submitButton = document.getElementById("submit");
+let display = document.getElementById("display");
+
+function main(){
+  let n = parseInt(numberBox.value);
+  display.innerHTML = sumOfPrimes(n);
+}
+
+submitButton.addEventListener("click", main);
