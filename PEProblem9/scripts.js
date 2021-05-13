8f8cce1942b5b7ee255f38b3c5a1039e16e5f306
Alec <38861973+ScalarShift@users.noreply.github.com>
2021-05-12 20:24:15 -0700

    Fix Solution 9

--- PEProblem9/scripts.js
+++ PEProblem9/scripts.js
@@ -1,18 +1,18 @@
 function generatePythTrip(n){
   let triples = []
-  for(let a=1; triples.length<n; a++){
-    for(let b=a; b<1000; b++){
-      for(let c=b; c<1000; c++){
+  for(let a=1; a<n; a++){
+    for(let b=a; b<n; b++){
+      for(let c=b; c<n; c++){
         if(a*a + b*b === c*c){
-          triples.push([a,b,c]);
+	  if(a + b + c === n){
+            triples.push([a,b,c]);
+          }
         }
       }
     }
   }
   return triples;
 }
-
-
 
 //-------------------------------------------------
 
@@ -22,7 +22,7 @@ let display = document.getElementById("display");
 
 function main(){
   let n = parseInt(numberBox.value);
-  display.innerHTML = largestProductOfNTerms(number, n);
+  display.innerHTML = generatePythTrip(n);
 }
 
 submitButton.addEventListener("click", main);
